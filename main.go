4a98a33c153b56c4868eea99a69e0d9c1f9ce334
saiaapiz <29090012+saiaapiz14@users.noreply.github.com>
2023-03-12 02:47:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,64 +17,71 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var client *whatsmeow.Client
-var recipientNumbers = []string{"601160564476@s.whatsapp.net", "60122412026@s.whatsapp.net"} // List of recipient numbers
+var (
+	WAClient *whatsmeow.Client
+
+	// List of recipient numbers
+	recipientNumbers = []string{
+		"601160564476@s.whatsapp.net",
+		"60122412026@s.whatsapp.net",
+	}
+)
 
 func eventHandler(evt interface{}) {
-	switch v := evt.(type) {
+	switch event := evt.(type) {
 	case *events.Message:
-		if !v.Info.IsFromMe {
-			for _, num := range recipientNumbers { //scan through recipient number list
-				if v.Message.GetConversation() == "/admin" { //check if the message use admin keyword
-					if v.Info.Sender.String() == num { // Check if the incoming message is from one of the recipient numbers
-						fmt.Println("PESAN DITERIMA DARIPADA ADMIN!", v.Message.GetConversation()) //show message in terminal that admin sent the message
-						client.SendMessage(v.Info.Sender, "", &waProto.Message{                    //send message response to the keyword
-							Conversation: proto.String("YA TUAN APA SAYA BOLEH BANTU"), //content of the message
-						})
-						break //exit after running function
-					} else {
-						fmt.Println("PESAN DITERIMA DARIPADA USER!", v.Message.GetConversation())
-						client.SendMessage(v.Info.Sender, "", &waProto.Message{
-							Conversation: proto.String("Pesan ini automatik, menggunakan GO!. Anda mengirim pesan: " + v.Message.GetConversation()),
-						})
-						break
-
-					}
-				} else if v.Message.GetConversation() != "" {
-					if v.Info.Sender.String() != num { // Check if the incoming message is from one of the recipient numbers
-						fmt.Println("PESAN DITERIMA DARIPADA USER!", v.Message.GetConversation())
-						client.SendMessage(v.Info.Sender, "", &waProto.Message{
-							Conversation: proto.String("Pesan ini automatik, menggunakan GO!. Anda mengirim pesan: " + v.Message.GetConversation()),
-						})
-						break
-
-					}
+		if msg := event.Message.GetConversation(); !event.Info.IsFromMe {
+			var isInRecipient bool
+			for _, n := range recipientNumbers {
+				if event.Info.Sender.String() == n {
+					isInRecipient = true
 				}
 			}
+
+			log := waLog.Stdout(fmt.Sprintf("[UpdateHandler][%s][IsRecipient: %t]", event.Info.Sender, isInRecipient), "DEBUG", true)
+			log.Infof(`Received new message -> "%s"`, msg)
+
+			var replyStr string
+
+			switch msg {
+			case "/admin":
+				if isInRecipient {
+					replyStr = "YA TUAN APA SAYA BOLEH BANTU"
+				}
+			default:
+				replyStr = "Pesan ini automatik, menggunakan GO!. Anda mengirim pesan: " + msg
+			}
+
+			WAClient.SendMessage(event.Info.Sender, "", &waProto.Message{
+				Conversation: proto.String(replyStr),
+			})
 		}
 	}
 }
 
 func main() {
 	dbLog := waLog.Stdout("Database", "DEBUG", false)
+
 	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
 	container, err := sqlstore.New("sqlite3", "file:wsap.db?_foreign_keys=on", dbLog)
 	if err != nil {
 		panic(err)
 	}
+
 	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
 	deviceStore, err := container.GetFirstDevice()
 	if err != nil {
 		panic(err)
 	}
-	clientLog := waLog.Stdout("Client", "DEBUG", false)
-	client = whatsmeow.NewClient(deviceStore, clientLog)
-	client.AddEventHandler(eventHandler)
 
-	if client.Store.ID == nil {
+	WAClient = whatsmeow.NewClient(deviceStore, waLog.Stdout("Client", "DEBUG", false))
+	WAClient.AddEventHandler(eventHandler)
+
+	// Handle client auth
+	if WAClient.Store.ID == nil {
 		// No ID stored, new login
-		qrChan, _ := client.GetQRChannel(context.Background())
-		err = client.Connect()
+		qrChan, _ := WAClient.GetQRChannel(context.Background())
+		err = WAClient.Connect()
 		if err != nil {
 			panic(err)
 		}
@@ -92,16 +99,15 @@ func main() {
 		}
 	} else {
 		// Already logged in, just connect
-		err = client.Connect()
-		if err != nil {
+		if err = WAClient.Connect(); err != nil {
 			panic(err)
 		}
 	}
 
 	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 
-	client.Disconnect()
+	WAClient.Disconnect()
 }
